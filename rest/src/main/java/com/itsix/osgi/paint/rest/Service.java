4eb48e718a7d0f2d29afbbbb5ebdb7c9c7a7b499
Bogdan Constantin <bogdan.alexandru@gmail.com>
2015-12-14 18:42:46 +0200

    Fixed REST & JSON compile time dependencies and service root resource path.

--- rest/src/main/java/com/itsix/osgi/paint/rest/Service.java
+++ rest/src/main/java/com/itsix/osgi/paint/rest/Service.java
@@ -2,23 +2,30 @@ package com.itsix.osgi.paint.rest;
 
 import java.util.Collection;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jackson.map.annotate.JsonView;
+
 import com.itsix.osgi.paint.api.PaintApi;
 import com.itsix.osgi.paint.api.Shape;
 
+@Path("/")
 public class Service {
 
+    @Context
+    ServletContext context;
+
+    @JsonView(Shape.class)
     @GET
     @Path("shapes")
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Shape> listShapes(@Context HttpServletRequest request) {
-        PaintApi api = (PaintApi) request.getServletContext()
+    public Collection<Shape> listShapes() {
+        PaintApi api = (PaintApi) context
                 .getAttribute(PaintApi.class.getName());
         return api.listShapes();
     }
